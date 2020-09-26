bf2da6f87029349edaffc67e195c9186d90bb916
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-09-25 21:02:55 -0400

    Create greater_than_100.js

---
+++ demo/greater_than_100.js
@@ -0,0 +1,17 @@
+function fnIsGreaterThan100_loop(arr) { 
+  
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] > 100) {
+      return true; 
+   } 
+   
+ } 
+ 
+ return false;
+ } 
+ 
+ fnIsGreaterThan100_loop(930,60,90,120]);
+ fnIsGreaterThan100_loop([30,60,90]);
+ 
+ 
+ 
